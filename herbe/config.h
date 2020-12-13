24e4cb89d95c8a1f57c867c237b6f2362bd3d7c0
spleaner1 <spencerpeters04@gmail.com>
2020-12-12 22:21:43 -0500

    added herbe files and added aliases to shrc

---
+++ herbe/config.h
@@ -0,0 +1,19 @@
+static const char *background_color = "#101010";
+static const char *border_color = "#3d3d3d";
+static const char *font_color = "#999999";
+static const char *font_pattern = "SF Mono:size=12";
+static const unsigned line_spacing = 5;
+static const unsigned int padding = 15;
+
+static const unsigned int width = 150;
+static const unsigned int border_size = 2;
+static const unsigned int pos_x = 30;
+static const unsigned int pos_y = 60;
+
+enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
+enum corners corner = TOP_RIGHT;
+
+static const unsigned int duration = 3; /* in seconds */
+
+#define DISMISS_BUTTON Button1
+#define ACTION_BUTTON Button3
