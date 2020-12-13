04934949d821595bf3233bf90381a53baafd22b5
spleaner1 <spencerpeters04@gmail.com>
2020-12-13 01:39:39 -0500

    updated volume bar

--- bin/barvol.sh
+++ bin/barvol.sh
@@ -2,11 +2,17 @@
 
 volume() {
 	VOLUME=$(pamixer --get-volume-human)
+	if [ $VOLUME = muted ]
+	then
+		REALVOLUME=" 0%"
+	else
+		REALVOLUME=$(pamixer --get-volume-human)
+	fi
 
-	echo -n "volume is $VOLUME"
+	echo -n "volume is $REALVOLUME"
 }
 
 while true; do
 	echo "%{l}%{F#999999}%{B#101010} $(volume) %{F-}%{B-}"
-	sleep 1
+	sleep .1
 done
