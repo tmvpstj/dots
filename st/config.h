0498a0e49fe86df909706c96e9876ea1bab803de
tmvpstj <spencerpeters04@gmail.com>
2023-10-08 16:21:52 -0400

    updated latex setup

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Berry:pixelsize=14:antialias=true:autohint=true";
+static char *font = "SF Mono:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 50;
 
 /*
